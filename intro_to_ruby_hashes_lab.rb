42b71336d7dc4722986cb3eb9dd95c047d7ed24a
Zeshan Raja <zraja4402@gmail.com>
2019-11-08 20:36:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ hash = {
     },
     :names => {
       :reading_railroad => 
-      :pennsylvania_railroad
+      :pennsylvania_railroad =>
       :b_and_o_railroad =>
       :shortline =>
     }
