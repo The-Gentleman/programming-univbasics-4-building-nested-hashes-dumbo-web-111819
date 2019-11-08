ee0a72ad8ac95a6bc1c040512e12dfa5e90ac26c
Zeshan Raja <zraja4402@gmail.com>
2019-11-08 20:46:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,7 +44,7 @@ hash = {
     },
     :names => {
       :reading_railroad => {
-        :mortgage_value = $100
+        :mortgage_value => $100
       },
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {}, 
